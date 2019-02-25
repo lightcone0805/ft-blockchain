64e4772085f6100a6fc0d23f957fa91187400e84
wengod <1025629909@qq.com>
2019-02-25 23:26:01 +0800

    update cors header in express

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ var initHttpServer = () => {
     app.use(bodyParser.json());
     app.all('*', function(req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With,authorization");
         res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
         res.header("X-Powered-By",' 3.2.1');
         res.header("Content-Type", "application/json;charset=utf-8");
